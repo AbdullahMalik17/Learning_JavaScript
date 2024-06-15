177ba4df35509c1ad7ae83c4bf112949acc6ded0
Abdullah Ather <abdullahather272@gmail.com>
2024-06-15 14:58:03 +0500

    Added Case-Sensitive file

--- Comment.js
+++ Comment.js
@@ -1,1 +1,14 @@
+/*
 
+                  <------Comment------->
+They also help the programmer to understand our code .They 
+are written in natural language .They are not rxecuted in the 
+program.
+
+Types:
+1.Single line comment
+2.multi comment
+*/
+
+
+    
