f8203a0c21bfdb1bcca8527b3661efbaa941e07b
Abdullah Ather <abdullahather272@gmail.com>
2024-06-19 19:04:34 +0500

    Added parseFloat File

---
+++ Conversion-string-to-num/parseFloat.js
@@ -0,0 +1,12 @@
+/*
+<----------parseFloat----------->
+It is used to convert decimal value(string ) to number.
+It's syntax as follows:
+var var-name=parseFloat(______)
+
+*/
+//For Example:
+var height=prompt("What is your height?","6.8");
+var num=7;
+var sum=parseFloat(height)+num;
+console.log(sum);
