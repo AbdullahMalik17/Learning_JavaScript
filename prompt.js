7271c5280fcc7928dbc2ba3c532b2fcfdd5ea150
Abdullah Ather <abdullahather272@gmail.com>
2024-06-19 16:20:22 +0500

    Added Prompt File

---
+++ prompt.js
@@ -0,0 +1,13 @@
+/* 
+
+<------Prompt-------->
+It is used to get input from user .Two values are set in it ,
+one is asked from user and another we set as default.
+It's syntax as follows:
+
+var var-name= prompt("input");
+
+*/
+//For example:
+var name=prompt("What is your name?");
+document.write(name);
