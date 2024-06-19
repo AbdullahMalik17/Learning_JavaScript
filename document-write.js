9e4c978e88bb623c89fe4f4f2e8d47ca519b1884
Abdullah Ather <abdullahather272@gmail.com>
2024-06-19 14:43:41 +0500

    Added Document-write File

--- document-write.js
+++ document-write.js
@@ -1,1 +1,11 @@
+/*
 
+<-----------document.write----------->
+It is used to show output on the screen .
+It's syntax as follows:
+document.write(__);
+
+*/
+// For Example:
+document.write("Hello World");
+ 
