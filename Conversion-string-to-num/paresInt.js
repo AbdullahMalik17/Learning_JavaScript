b83ad09f6a7287c64d1a15e686fe228d23c6bb27
Abdullah Ather <abdullahather272@gmail.com>
2024-06-19 16:38:22 +0500

    Added Conversion Folder

---
+++ Conversion-string-to-num/paresInt.js
@@ -0,0 +1,12 @@
+/*
+
+<--------parseInt()--------->
+It is  used to convert string into number for addition.
+It is used after prompt.
+*/
+//For Example:
+var age=prompt("What is your age?","5");
+var n=6;
+var sum=parseInt(age)+n;
+console.log(sum);
+
