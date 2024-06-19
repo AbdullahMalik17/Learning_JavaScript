d863de12749ece474b454b0599c3f0a5de7dc3d7
Abdullah Ather <abdullahather272@gmail.com>
2024-06-19 17:07:11 +0500

    Added operator folder

---
+++ Operator/Assignment-operator.js
@@ -0,0 +1,16 @@
+/*
+
+
+<---------Assignment Operator -------->
+
+It is used to assign a value to one value to variable.
+Or, variable to another variable.
+
+*/
+// For Example:
+var c=10; 
+var a=5;
+var b=a;
+console.log(a);
+console.log(b);
+console.log(c);
