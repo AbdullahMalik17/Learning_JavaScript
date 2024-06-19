ffc8df7feafaf916f7a93886da22efd2cdf996e1
Abdullah Ather <abdullahather272@gmail.com>
2024-06-19 14:37:40 +0500

    Added Script File

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 It is used to integratye a javascript file with html page.
 For example:
 <script src=""></script>
-src is used to give path of javascript file 
+src is used to give path of javascript file .
 
 */
     
