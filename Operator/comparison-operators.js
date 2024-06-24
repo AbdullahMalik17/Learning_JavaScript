ee023ef06689115834814353b4c048ff5e105efb
Abdullah Ather <abdullahather272@gmail.com>
2024-06-24 21:17:55 +0500

    Added comparison operators

---
+++ Operator/comparison-operators.js
@@ -0,0 +1,32 @@
+
+/*
+
+<---------Comparison Operators------->
+They are be used to compare a one value with another .
+ Uses:- 
+     They are also used in logical operators and if-else statments ...etc .
+      
+Some of the comparison  operators are as follows
+
+ 1. Check Equality               | == 
+ 2. Check Equality and Data type | ===
+ 3. Not Equal to                 | !=
+ 4. Not equal to and Data type   | !==
+ 5. Greater than                 | >
+ 6. Greater than and Equal to    | >=
+ 7. Less than                    | <
+ 8. Less than and Equal to       | <=
+
+ Result :-
+         They give the result in  boolean proposition.
+
+*/
+// For Example:
+let a = 6;
+let b = "6";
+console.log(a==b);   // return true 
+console.log(a===b);  // return false
+console.log(a>b);    // return false
+console.log(a>=b);   // return false 
+console.log(a<b);    // return false
+console.log(a<=b);   // return false
