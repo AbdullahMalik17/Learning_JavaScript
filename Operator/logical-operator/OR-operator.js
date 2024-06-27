c257d177c8e50ce36427c4efee91526d4dbc071a
Abdullah Ather <abdullahather272@gmail.com>
2024-06-27 14:48:35 +0500

    added OR operator

---
+++ Operator/logical-operator/OR-operator.js
@@ -0,0 +1,19 @@
+
+/*
+
+<----------OR operator---------->
+It gives the result true when any value is true.
+It works on multiple expression.It is represented with ||
+
+*/
+//For example:-
+let score=prompt("What is your score in match:");
+if(score>70||score<80){
+    console.log(Grade=B);
+}
+console.log(true||true);     //result true
+console.log(false||true);    //result true
+console.log(true||false||false);  //result true 
+console.log(false||false);      //result false
+
+
