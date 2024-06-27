b0d18aafc7b98c14d648ed6fad0982e131ba8b9f
Abdullah Ather <abdullahather272@gmail.com>
2024-06-27 14:33:16 +0500

    Added Logical Operator Folder

---
+++ Operator/logical-operator/AND-operator.js
@@ -0,0 +1,13 @@
+/*
+
+<--------AND operator--------->
+It gives result true when all the expression are true .
+ It works on multiple expession .It is represent with &&.
+
+*/
+//For example:-
+
+console.log(true&&true);       //result true 
+console.log(3>2&&6>5);        //result true
+console.log(true&&true&&true);//result true  
+console.log(false&&true&true);//result false
---
+++ Operator/logical-operator/explantion.js
@@ -0,0 +1,13 @@
+
+/*
+
+<-----------Logical operators----------->
+They are used to perform logic on boolean proposition or expression.
+They are applied in control statements 
+(like..if-statement ,if-else-statement and switch).
+ 
+There are three types of operators:-
+1. AND operator
+2. OR operator
+3. NOT operator
+
