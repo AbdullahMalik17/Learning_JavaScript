9d8c453c18265dd994febe82ce2afe42768f08c6
Abdullah Ather <abdullahather272@gmail.com>
2024-06-27 15:30:18 +0500

    Added Conditional Statement Folder

---
+++ Conditional-statement/Readme.md
@@ -0,0 +1,8 @@
+### Conditional Statement
+
+They are used to rxecute a statement on the basis of condition.
+It has two types as follows:
+- if-statement
+- if-else statement
+
+Their explanation is written in files.
---
+++ Conditional-statement/if-statement.js
@@ -0,0 +1,20 @@
+/*
+
+<-------- if statement------->
+It executes a part of program when condition 
+turns to be true otherwise set of statements are run.
+
+It's syntax as follows:-
+if(condition){
+set of statement 
+}
+
+condition:- 
+          It is an expression which turn result in boolean expression.
+
+*/
+// For Example:-
+ var b=6;
+ if(b>=6){
+    console.log(Hello World);
+ }
