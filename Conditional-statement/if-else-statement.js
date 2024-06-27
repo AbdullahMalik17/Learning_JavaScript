185b3728f2381c2531b58732f39b0c47c7982264
Abdullah Ather <abdullahather272@gmail.com>
2024-06-27 15:47:48 +0500

    Added if else statement file

---
+++ Conditional-statement/if-else-statement.js
@@ -0,0 +1,23 @@
+
+/*
+
+<---------if else statement------->
+If the condition turn to be false then ,the 
+other set of statements are executed in else part.
+else needs if but if statemet not needs else.
+
+Its syntax as follows:-
+if(condition){
+set of statements
+} else {
+set of statements 
+}
+
+*/
+//For example:-
+let age = prompt("What is your age:");
+if (age<18){
+    document.write(Child);
+} else {
+    document.write(Young);
+}
