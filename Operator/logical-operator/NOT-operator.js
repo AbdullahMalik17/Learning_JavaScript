0fd0790152784d9316db0c82e706041c3073ffe1
Abdullah Ather <abdullahather272@gmail.com>
2024-06-27 15:04:37 +0500

    Added Not operators

---
+++ Operator/logical-operator/NOT-operator.js
@@ -0,0 +1,11 @@
+/*
+
+<----------NOT Operator---------->
+It is a negate operator.If the value is true, it gives
+false.It is represented with !
+
+*/
+// For example:-
+console.log(!true);     //return false
+console.log(!false);    //return true
+
