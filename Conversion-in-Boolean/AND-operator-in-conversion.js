fa7afb98a43713dcadd368939e9d94800afeee35
Abdullah Ather <abdullahather272@gmail.com>
2024-07-03 16:53:08 +0500

    Added AND operators in conversion file

---
+++ Conversion-in-Boolean/AND-operator-in-conversion.js
@@ -0,0 +1,15 @@
+
+/*
+
+<-------------AND Operators in Conversion------------->
+1. If all the operands are non booleans and true , it returns
+   the last operands.
+2. If one operand is false in non boolean value, then it returns the 
+    false value.
+
+*/
+// For example:-
+var b="cat"&&"dog";
+var c=0&&"dogs"
+console.log(b)
+console.log(c)
