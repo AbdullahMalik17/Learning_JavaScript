c1b06fc29b54c1482a3d0ab2dc15111e4c6869c0
Abdullah Ather <abdullahather272@gmail.com>
2024-07-03 17:33:19 +0500

    Added for loop file

--- Loops/for-loop.js
+++ Loops/for-loop.js
@@ -2,4 +2,14 @@
 /*  
 <---------for loop---------->
 They are used to repeat the statement for any time .
-Its syn
+Its syntax as follows:-
+
+for(initialization;condition;increment/decrement){
+     body of for loop
+}
+*/
+// For example:-
+var name =["Abdullah","Haleema","Hamna","Amna","Mother Bushra","Father Athar"];
+for (var i=0;i<name.length;i++){
+    console.log(name[i])
+}
