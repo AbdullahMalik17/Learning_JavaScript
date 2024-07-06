0337ab1ad5978dfbcbb71e6c6a92f2af1b902c90
Abdullah Ather <abdullahather272@gmail.com>
2024-07-06 15:47:35 +0500

    Added indexOf file

---
+++ string/indexOf.js
@@ -0,0 +1,16 @@
+/*
+<--------indexOf()------->
+It is check the element is present in the array .
+If element is present init , it gives the index of element 
+in the array.
+The working of last index is same as the LastindexOf.It checks the element from last.
+
+It's syntax as follows:-
+ variable-name.indexOf(value)
+
+*/
+// for example:-
+
+let a = "To be or not to be"
+console.log(a.indexOf("or"))    // output 6
+console.log(a.indexOf("not"))   // output 9
