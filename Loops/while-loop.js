834164b4e8bced214379588b060bfa593befcc80
Abdullah Ather <abdullahather272@gmail.com>
2024-07-06 12:59:23 +0500

    Updated while loop file

---
+++ Loops/while-loop.js
@@ -0,0 +1,21 @@
+
+/*   
+<-----------while loop------------->
+It is a type of loop that repeat a code of statement untill 
+the condition isturns to be true.
+
+It's syntax as follows:
+while(condition){
+block of code
+increment(optional)
+}
+
+If we do not put the increment in it.It becomes infinite loop
+
+*/
+// For example:
+let i=1;
+while(i<10){
+    console.log("I = "+i)
+    i++
+}
