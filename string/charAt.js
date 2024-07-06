29a61f1c654637536473f163b921400dc1a53b77
Abdullah Ather <abdullahather272@gmail.com>
2024-07-06 16:01:40 +0500

    Added charAt() file

---
+++ string/charAt.js
@@ -0,0 +1,15 @@
+/*
+            <--------charAt()--------->
+ It is used to access the element in the string on 
+v the basis of index.
+
+
+ It's syntax as folows:-
+   variable-name.charAt( index no. )
+
+*/
+// For example:-
+
+let a="To be or not to be"
+console.log(a.charAt(4))    // output e
+console.log(a.charAt(50))    //output nothing
