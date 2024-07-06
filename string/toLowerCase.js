2ff797b60e0d878786fc15760474facd99f8ab94
Abdullah Ather <abdullahather272@gmail.com>
2024-07-06 14:24:37 +0500

    Added toLowerCase File

---
+++ string/toLowerCase.js
@@ -0,0 +1,11 @@
+/*  
+<-------toLowerCase()-------->
+  It is used to make all the element of string, be small.
+
+  It's syntax as follows:-
+  variable-name.toLowerCase()
+
+*/
+// For Example:-
+ let a = "HEy,  ABDullah"
+ console.log(a.toLowerCase())    // output hey, abdullah 
