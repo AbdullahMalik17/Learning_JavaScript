198bed26dd5fa37fe7f787a59d668ca5febe2fb3
Abdullah Ather <abdullahather272@gmail.com>
2024-07-06 15:19:09 +0500

    Added toUpperCase()  file

---
+++ string/toUpperCase.js
@@ -0,0 +1,12 @@
+/*
+<---------toUpperCase---------->
+  It is used to make the element of string large.
+
+  It's syntax as folows:-
+
+  variable-name.toUpperCase()
+
+*/
+// For example:-
+ let a="ABDullAh"
+ console.log(a.toUpperCase())   // output ABDULLAH
