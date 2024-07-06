9ad6dab08090a23370dcbcb08b121becbc312110
Abdullah Ather <abdullahather272@gmail.com>
2024-07-06 19:48:52 +0500

    Added Escape character file

---
+++ string/Escape-character.js
@@ -0,0 +1,16 @@
+/*
+    <---------Escape Character----------->
+    When we write a more content in the string ,we need some 
+    character are called escape character.
+    
+    Some of the escape character are as follows:
+
+    \n          |           new line
+    \t          |           collection of 8 spaces
+    \'          |           for single quote
+    \"          |           for double quote
+
+*/
+// For Example:-
+
+console.log("I am Abdullah.\nI read in first year.\nI am 17 year of age.");
