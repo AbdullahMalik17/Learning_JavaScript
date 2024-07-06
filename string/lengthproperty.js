5ea82b2e69b23b3099cc23cdfe8274db3cafb559
Abdullah Ather <abdullahather272@gmail.com>
2024-07-06 14:14:50 +0500

    Added length property file

---
+++ string/lengthproperty.js
@@ -0,0 +1,14 @@
+/*
+<---------length property---------.
+It is used to find the values of element in 
+the string.
+
+It's syntax as follows:
+          variable-name.length
+
+*/
+// For example 
+
+let conversation='Hello World'
+let a=conversation.length
+console.log(a)      // output 11
