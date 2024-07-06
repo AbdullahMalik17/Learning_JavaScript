413972358eff827cc09300006f170548c7b997a2
Abdullah Ather <abdullahather272@gmail.com>
2024-07-06 15:34:50 +0500

    Added slice file

---
+++ string/slice.js
@@ -0,0 +1,14 @@
+/* 
+<----------slice()----------->
+It is used to take the specific values of string . If we use
+negative index in the functiuon(it works oppoit side of counting)
+
+It's syntax as follow:-
+variable-name.slice(start of index,end of index);
+
+*/
+// For example:-
+
+let a="Hello World"
+console.log(a.slice(3,10))   //output lo Worl
+console.log(a.slice(3))      //output lo World
