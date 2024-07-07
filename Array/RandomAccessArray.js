f242a912ce4a86861063066929399b2cdf3812e4
Abdullah Ather <abdullahather272@gmail.com>
2024-07-07 13:13:46 +0500

    Added Random access Array File

---
+++ Array/RandomAccessArray.js
@@ -0,0 +1,15 @@
+/*
+
+    <--------Random Access Array-------->
+ Array can be access by its index. Index are started 
+  from 0 . Indexes are written in square brackets
+
+*/
+// For example:-
+let name = ["Abdullah","Haleema","Hamna"];
+console.log(name[0])  // output Abdullah
+console.log(name[1])  // output Haleema
+name[3]="Amna"
+console.log(name)   // output [ 'Abdullah', 'Haleema', 'Hamna', 'Amna' ]
+name[0]="Athar"
+console.log(name)   // output [ 'Athar', 'Haleema', 'Hamna', 'Amna' ]
