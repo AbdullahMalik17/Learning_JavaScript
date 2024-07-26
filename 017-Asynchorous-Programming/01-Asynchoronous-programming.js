a30bc3df9e412fa8f513d046c6946f0952830818
Abdullah Ather <abdullahather272@gmail.com>
2024-07-26 18:38:24 +0500

    Updated Lesson

---
+++ 017-Asynchorous-Programming/01-Asynchoronous-programming.js
@@ -0,0 +1,28 @@
+/*
+<--------Synchoronous Programming-------->
+   It means all the program is executed in a given sequence. Each instruction is wait 
+   the previous instruction.
+   For example:-
+        A simple way of Programming is called Synchorous Programming.
+        When we fetch data from API , It require some time to send data in case the whole execution is stopped in 
+        synchorous programming.
+       
+<-------Asynchoronous Programming--------->
+   Due to Synchorous Programming , all the executin is stopped for some second.
+   To meet this problem we use Asynchoronous Programming.
+    Function:-
+         setTimeOut is an asynchorous function . It stop the function for that time which we want and runs the 
+         below code.
+         
+ <------setTimeout------->
+   It's syntax as follows:-
+      setTimeout(function-name, time in millisecond)
+
+ */
+console.log("Data 1");
+console.log("Data 2");
+setTimeout(() =>{
+    console.log("Data 3")   // It is executed after 5 seconds , below is executed.
+},5000);     
+console.log("Data 4")
+console.log('Data 5')   
