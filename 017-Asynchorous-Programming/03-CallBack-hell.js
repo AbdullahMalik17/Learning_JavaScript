e38f7766a571683c8adaedd0ef0f98bbd5d6aaef
Abdullah Ather <abdullahather272@gmail.com>
2024-07-26 19:56:15 +0500

    Updated Lesson

---
+++ 017-Asynchorous-Programming/03-CallBack-hell.js
@@ -0,0 +1,25 @@
+/*
+    <------Call Back Hell------>
+    When we use different call back , it is difficult to understand to us. 
+    This ius callerd Callback Hell.
+
+*/
+   function getId(data,data2){
+    setTimeout(() => {
+        console.log("Data"+data)
+        if (data2){
+            data2()
+        }
+    },3000) 
+   } 
+   getId(1,() =>{
+    console.log("Fetching Data2 ......")
+    getId(2,() =>{
+        console.log("Fetching Data3.....")
+        getId(3,() =>{
+            console.log("fetching data4......")
+            getId(4)
+     })
+    })
+   })
+   // It is complex to be understandable called callback hell 
